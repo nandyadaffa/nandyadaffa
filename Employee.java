30e17675e9654231cb67d293dc06571f88eb1e22
nandyadaffa <145904135+nandyadaffa@users.noreply.github.com>
2024-01-02 13:25:40 +0700

    Employee.java

--- Employee.java
+++ Employee.java
@@ -69,21 +69,21 @@ class Main {
     public static void main(String[] args) {
         // Create instances of Manager, Developer, and Designer
         System.out.println("===== Manager =====");
-        Manager manager = new Manager("Sri Hastuti");
+        Manager manager = new Manager("Nandya M Daffa");
         System.out.println("Manager Name: " + manager.getName());
         manager.work(); // Memanggil metode work pada manager
         
         System.out.println();
         
         System.out.println("===== Developer =====");
-        Developer developer = new Developer("Nurisa");
+        Developer developer = new Developer("Ayu");
         System.out.println("Developer Name: " + developer.getName());
         developer.work(); // Memanggil metode work pada developer
         
         System.out.println();
         
         System.out.println("===== Designer =====");
-        Designer designer = new Designer("Tuti");
+        Designer designer = new Designer("Dede");
         System.out.println("Designer Name: " + designer.getName());
         designer.work();
     }
